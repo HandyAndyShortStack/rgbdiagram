0aabb3a7477550ce1a4fe3f00635d131b6de4d73
Andy Short <andynshort@gmail.com>
2015-02-18 13:34:40 -0800

    store board data

--- all_boards.js
+++ all_boards.js
@@ -1,17 +1,29 @@
 var Board = require('./board.js');
 var nextPermutation = require('./next_permutation.js');
 
-module.exports = allBoards
+module.exports = {
+  all: allBoards,
+  unique: uniqueBoards,
+  getUnique: getUniqueEntry,
+  equivalencyHash: equivalencyHash,
+  reverseEquivalencyHash: reverseEquivalencyHash,
+  uniqueBoardHash: uniqueBoardHash
+};
+
+var cache = {};
 
 function allBoards() {
+  if (cache.allBoards) {
+    return cache.allBoards;
+  }
   var a = [0, 0, 0, 1, 1, 1, 2, 2, 2];
-  var allBoards = [a];
+  var boards = [a];
   var notDone = nextPermutation(a);
   while (notDone) {
-    allBoards.push(a.slice(0));
+    boards.push(a.slice(0));
     notDone = nextPermutation(a);
   }
-  allBoards = allBoards.map(function(arr) {
+  boards = boards.map(function(arr) {
     return new Board(arr.map(function(num) {
       if (num === 0) {
         return 'R';
@@ -22,9 +34,19 @@ function allBoards() {
       }
     }));
   });
+  cache.allBoards = boards;
+  return boards
+}
+
+function uniqueBoards() {
+  if (cache.uniqueBoards) {
+    return cache.uniqueBoards;
+  }
+  var memo = null;
+  var _allBoards = allBoards();
   var boards = [];
-  for (var i = 0; i < allBoards.length; i += 1) {
-    var board = allBoards[i];
+  for (var i = 0; i < _allBoards.length; i += 1) {
+    var board = _allBoards[i];
     var isEquivalent = boards.reduce(function(accum, current) {
       return accum || board.isEquivalentTo(current);
     }, false);
@@ -32,5 +54,55 @@ function allBoards() {
       boards.push(board);
     }
   }
+  cache.uniqueBoards = boards;
   return boards;
+}
+
+function equivalencyHash() {
+  if (cache.equivalencyHash) {
+    return cache.equivalencyHash;
+  }
+  var _uniqueBoards = uniqueBoards();
+  var hsh = {};
+  for (var i = 0; i < _uniqueBoards.length; i += 1) {
+    var board = _uniqueBoards[i];
+    hsh[board.toString()] = board.allEquivalencies();
+  }
+  cache.equivalencyHash = hsh;
+  return hsh;
+}
+
+function reverseEquivalencyHash() {
+  if (cache.reverseEquivalencyHash) {
+    return cache.reverseEquivalencyHash;
+  }
+  var _equivalencyHash = equivalencyHash();
+  var hsh = {};
+  for (var key in _equivalencyHash) {
+    var boardlist = _equivalencyHash[key];
+    for (var i = 0; i < boardlist.length; i += 1) {
+      var board = boardlist[i];
+      hsh[board.toString()] = key;
+    }
+  }
+  cache.reverseEquivalencyHash = hsh;
+  return hsh;
+}
+
+function uniqueBoardHash() {
+  if (cache.uniqueBoardHash) {
+    return cache.uniqueBoardHash;
+  }
+  var _uniqueBoards = uniqueBoards();
+  var hsh = {}
+  for (var i = 0; i < _uniqueBoards.length; i += 1) {
+    var board = _uniqueBoards[i];
+    hsh[board.toString()] = board;
+  }
+  cache.uniqueBoardHash = hsh;
+  return hsh;
+}
+
+function getUniqueEntry(board) {
+  return uniqueBoardHash()[reverseEquivalencyHash()[board.toString()]]
 }
